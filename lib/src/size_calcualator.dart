29f18ff37a64fcc78b748395df0968b7d507dd62
unknown <zamani3509@gmail.com>
2022-07-19 20:03:54 +0430

    create SizeCalculator class

--- lib/src/size_calcualator.dart
+++ lib/src/size_calcualator.dart
@@ -1,24 +1,27 @@
 import 'size.dart';
 
 class SizeCalculator {
-  SizeCalculator(this._size)height=_size. {
+  SizeCalculator(this._size) {
     initFields();
   }
   final Size _size;
-  double height;
-  double width;
+
+  late double height;
+  late double width;
   late double top;
   late double left;
   late double bottom;
   late double right;
 
   void initFields() {
-    final double newTop = initialPosition.dy - height / 2;
-    final double newBottom = areaHeight - height - newTop;
-    final double newLeft = initialPosition.dx - (width / 2);
-    final double newRight = (areaWidth - width) - newLeft;
+    height = _size.height;
+    width = _size.width;
+    double newTop = _size.initialPosition.dy - height / 2;
+    double newBottom = _size.areaHeight - height - newTop;
+    double newLeft = _size.initialPosition.dx - (width / 2);
+    double newRight = (_size.areaWidth - width) - newLeft;
 
-    // init top&bottom
+    // init top & bottom
     if (newTop < 0) {
       bottom = newBottom + newTop;
     } else if (newBottom < 0) {
@@ -27,7 +30,7 @@ class SizeCalculator {
       bottom = newBottom;
       top = newTop;
     }
-    // init left&right
+    // init left & right
     if (newLeft < 0) {
       right = newRight + newLeft;
     } else if (newRight < 0) {
