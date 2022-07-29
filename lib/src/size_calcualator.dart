8c0b707433328c49e36cd399aa712e37a2057621
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-07-29 20:44:09 +0430

    Add setSize and other size calulator functions

--- lib/src/size_calcualator.dart
+++ lib/src/size_calcualator.dart
@@ -1,10 +1,7 @@
 import 'final_size.dart';
 
 class SizeCalculator {
-  SizeCalculator(this._size) {
-    initFields();
-  }
-  final FinalSize _size;
+  late final FinalSize _size;
 
   late double height;
   late double width;
@@ -13,7 +10,8 @@ class SizeCalculator {
   late double bottom;
   late double right;
 
-  void initFields() {
+  void initFields(FinalSize finalSize) {
+    _size = finalSize;
     height = _size.height;
     width = _size.width;
     double newTop = _size.initialPosition.dy - height / 2;
@@ -39,5 +37,42 @@ class SizeCalculator {
       right = newRight;
       left = newLeft;
     }
+  }
+
+  void setSize({
+    double? newTop,
+    double? newLeft,
+    double? newRight,
+    double? newBottom,
+  }) {
+    newTop = newTop ?? top;
+    newLeft = newLeft ?? left;
+    newRight = newRight ?? right;
+    newBottom = newBottom ?? bottom;
+    calculateWidgetSize(top: newTop, left: newLeft, bottom: newBottom, right: newRight);
+    if (checkTopBotMaxSize(newTop, newBottom)) {
+      top = newTop;
+      bottom = newBottom;
+    }
+    if (checkLeftRightMaxSize(newLeft, newRight)) {
+      left = newLeft;
+      right = newRight;
+    }
+    calculateWidgetSize(bottom: bottom, left: left, right: right, top: top);
+  }
+
+  bool checkTopBotMaxSize(final double newTop, final double newBottom) =>
+      (newTop >= 0 && newBottom >= 0) && (height >= _size.minHeight);
+  bool checkLeftRightMaxSize(final double newLeft, final double newRight) =>
+      (newLeft >= 0 && newRight >= 0) && (width >= _size.minWidth);
+
+  void calculateWidgetSize({
+    required final double top,
+    required final double left,
+    required final double bottom,
+    required final double right,
+  }) {
+    width = _size.areaWidth - (left + right);
+    height = _size.areaHeight - (top + bottom);
   }
 }
