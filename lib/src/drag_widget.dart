20caa5f8e3b0d2b15e844be7d930448949ba120f
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-08-08 20:27:12 +0430

    Add drag widget for more customizing

---
+++ lib/src/drag_widget.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:resizable_widget/src/drag_distance.dart';
+import 'package:resizable_widget/src/resizable_widget_controller.dart';
+
+import 'on_drag_types.dart';
+
+class DragWidget extends StatelessWidget {
+  final Widget child;
+  final double height;
+  final double width;
+  final DragDetailsType onDragType;
+  late final Alignment? alignment;
+  late final DragDetails onDrag;
+
+  DragWidget({
+    Key? key,
+    this.alignment,
+    required this.child,
+    required this.onDragType,
+    required this.height,
+    required this.width,
+  }) : super(key: key);
+
+  void init(ResizableWidgetController controller) {
+    onDrag = onDragType.getOnDragFunction(controller);
+    alignment ??= onDragType.alignment;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Align(
+      alignment: alignment!,
+      child: SizedBox(
+        width: width,
+        height: height,
+        child: DragDistance(onDrag: onDrag, child: child),
+      ),
+    );
+  }
+}
