355ab77194bcd61610a4ae7b83373b233f492802
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-08-09 20:24:25 +0430

    Refactor,get final sizes in widget Instead of  in controller

--- lib/src/resizable_widget.dart
+++ lib/src/resizable_widget.dart
@@ -1,22 +1,45 @@
 import 'package:flutter/material.dart';
 import 'package:resizable_widget/src/drag_widget.dart';
+import 'package:resizable_widget/src/final_size.dart';
 import 'resizable_widget_controller.dart';
 
 class ResizableWidget extends StatelessWidget {
   ResizableWidget({
-    Key? key,
+    super.key,
+    ResizableWidgetController? controller,
+    bool? showDragWidgets,
+    double? height,
+    double? width,
+    Offset? initialPosition,
+    double minWidth = 0.0,
+    double minHeight = 0.0,
+    required double areaHeight,
+    required double areaWidth,
     required this.child,
-    required this.controller,
     required this.dragWidgetsArea,
     required this.dragWidgetsList,
   }) {
+    height ??= areaHeight;
+    width ??= areaWidth;
+    initialPosition ??= Offset(width / 2, height / 2);
+    FinalSize finalSize = FinalSize(
+      areaHeight: areaHeight,
+      areaWidth: areaWidth,
+      height: height,
+      width: width,
+      minHeight: minHeight,
+      minWidth: minWidth,
+      initialPosition: initialPosition,
+    );
+    this.controller = controller ?? ResizableWidgetController();
+    this.controller.init(finalSize: finalSize, showDragWidgets: showDragWidgets);
     for (var element in dragWidgetsList) {
-      element.init(controller);
+      element.init(this.controller);
     }
   }
 
+  late final ResizableWidgetController controller;
   final Widget child;
-  final ResizableWidgetController controller;
   final Size dragWidgetsArea;
   final List<DragWidget> dragWidgetsList;
 
