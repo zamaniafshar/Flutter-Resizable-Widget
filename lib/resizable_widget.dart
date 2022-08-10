42c39a0ee098e3628bbaefb17da8cd939cda1e59
MohammadAminZamaniAfshar <zamani3509@gmail.com>
2022-08-10 11:32:04 +0430

    Export files

--- lib/resizable_widget.dart
+++ lib/resizable_widget.dart
@@ -1,1 +1,6 @@
 library resizable_widget;
+
+export 'src/drag_widget.dart';
+export 'src/resizable_widget.dart';
+export 'src/resizable_widget_controller.dart';
+export 'src/drag_details_types.dart';
