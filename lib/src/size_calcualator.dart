9f58d20499b8ea7c531f185e578f8b2f315a2213
MohammadAminZamani.afshar <zamani3509@gmail.com>
2022-09-05 18:48:12 +0430

    Documents SizeCalculator mixin

--- lib/src/size_calcualator.dart
+++ lib/src/size_calcualator.dart
@@ -10,6 +10,8 @@ class SizeCalculator {
   late double bottom;
   late double right;
 
+  /// Initializes the fields
+  /// It should called just once in widget lifecycle before [ResizableWidgetController] used
   void initFields(CommonSizes finalSize) {
     _size = finalSize;
     height = _size.height;
@@ -39,6 +41,10 @@ class SizeCalculator {
     }
   }
 
+  /// Get new sizes and Calculate widget size
+  /// If new sizes are safe then it will quantifies new sizes
+  /// Else new sizes ignoring
+  /// It called whenever user dragging
   void setSize({
     double? newTop,
     double? newLeft,
@@ -61,11 +67,17 @@ class SizeCalculator {
     calculateWidgetSize(bottom: bottom, left: left, right: right, top: top);
   }
 
+  /// Return True if [newTop] and [newBottom] are bigger than zero or equal it
+  /// and the [height] bigger than [minHeight] or equal it
   bool checkTopBotMaxSize(double newTop, double newBottom) =>
       (newTop >= 0 && newBottom >= 0) && (height >= _size.minHeight);
+
+  /// Return True if [newRight] and [newLeft] are bigger than zero or equal it
+  /// and the [width] bigger than [minWidth] or equal it
   bool checkLeftRightMaxSize(double newLeft, double newRight) =>
       (newLeft >= 0 && newRight >= 0) && (width >= _size.minWidth);
 
+  /// Calculate widget size by getting [top],[left],[bottom] and [right]
   void calculateWidgetSize({
     required double top,
     required double left,
